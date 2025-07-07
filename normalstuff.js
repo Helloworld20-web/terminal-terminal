9ed970e405147bbeabec2e4dbf44b81c10cdc192
Helloworld20-web <cadion.estrella68@gmail.com>
2025-07-07 15:53:23 +0400

    add more things
    cool

---
+++ normalstuff.js
@@ -0,0 +1,6 @@
+setInterval(change,200);
+function change() {
+    var colors = ["red","grey"];
+    document.getElementById("blink").style.color = colors;
+    document.getElementById("linkcolor").style.color = colors;
+}
